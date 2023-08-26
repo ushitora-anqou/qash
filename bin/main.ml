84a516e86a5163f08d9a9281ea81e1fb2da524d1
Ushitora Anqou <ushitora@anqou.net>
2023-08-26 11:40:48 +0900

    Use bulk insert to improve performance

--- bin/main.ml
+++ bin/main.ml
@@ -34,6 +34,11 @@ let () =
                 required & pos 0 (some string) None & info ~docv:"IN-FILE" [])
             $ Arg.(
                 required & pos 1 (some string) None & info ~docv:"OUT-FILE" []));
+        v (info "dump-csv")
+          Term.(
+            const dump_csv
+            $ Arg.(
+                required & pos 0 (some string) None & info ~docv:"IN-FILE" []));
         v (info "generate")
           Term.(
             const generate
--- lib/sql_writer.ml
+++ lib/sql_writer.ml
@@ -1,6 +1,13 @@
 open Lwt.Infix
 open Util
 
+let escape_single_quote s =
+  let buf = Buffer.create (String.length s) in
+  String.iter
+    (function '\'' -> Buffer.add_string buf "''" | c -> Buffer.add_char buf c)
+    s;
+  Buffer.contents buf
+
 module Store = struct
   let string_of_date (x : Model.date) =
     Printf.sprintf "%04d-%02d-%02d" x.year x.month x.day
@@ -211,6 +218,22 @@ ORDER BY t.created_at DESC, t.id DESC, p.id DESC
     Db.find Q.insert_transaction (string_of_date date, narration)
     |> raise_if_error
 
+  let insert_transactions (module Db : Caqti_lwt.CONNECTION)
+      (txs : Model.transaction list) =
+    (* FIXME: SQL injection *)
+    let query =
+      txs
+      |> List.map (fun (tx : Model.transaction) ->
+             Printf.sprintf "('%s', '%s')" (string_of_date tx.date) tx.narration)
+      |> String.concat " "
+      |> Printf.sprintf
+           {|INSERT INTO transactions (created_at, narration) VALUES %s RETURNING id|}
+    in
+    let open Caqti_request.Infix in
+    let open Caqti_type.Std in
+    Db.fold_s ((unit ->* int) query) (fun x xs -> Lwt.return_ok (x :: xs)) () []
+    |> raise_if_error
+
   let insert_posting (module Db : Caqti_lwt.CONNECTION) ~account_id
       ~transaction_id ~amount ~narration =
     Db.exec Q.insert_posting (account_id, transaction_id, amount, narration)
@@ -241,6 +264,144 @@ ORDER BY t.created_at DESC, t.id DESC, p.id DESC
           (Caqti_error.show e)
 end
 
+module StringMap = Map.Make (String)
+
+let rec list_last = function
+  | [] -> failwith "list_last"
+  | [ x ] -> x
+  | _ :: xs -> list_last xs
+
+let dump_account_id_map (model : Model.t) =
+  (* (partial) account name -> int *)
+  model.accounts
+  |> List.map (fun (a : Model.open_account) -> a.account)
+  |> List.flatten |> List.sort_uniq compare
+  |> List.mapi (fun i n -> (n, i))
+  |> List.to_seq |> StringMap.of_seq
+
+let dump_tag_id_map (model : Model.t) =
+  (model.accounts |> List.map (fun (a : Model.open_account) -> a.tags))
+  @ (model.transactions |> List.map (fun (t : Model.transaction) -> t.tags))
+  |> List.flatten |> List.sort_uniq compare
+  |> List.mapi (fun i n -> (n, i))
+  |> List.to_seq |> StringMap.of_seq
+
+let dump_account_records (model : Model.t) account_id_map =
+  (* (id, name, currency, parent_id, kind) list *)
+  model.accounts
+  |> List.map (fun (a : Model.open_account) ->
+         a.account
+         |> List.fold_left
+              (fun (parent_id, recs) name ->
+                let id = account_id_map |> StringMap.find name in
+                (Some id, (id, name, a.currency, parent_id, a.kind) :: recs))
+              (None, [])
+         |> snd)
+  |> List.flatten |> List.sort_uniq compare
+
+let dump_tag_records (model : Model.t) tag_id_map =
+  (* (id, name) list *)
+  (model.accounts |> List.map (fun (a : Model.open_account) -> a.tags))
+  @ (model.transactions |> List.map (fun (t : Model.transaction) -> t.tags))
+  |> List.flatten |> List.sort_uniq compare
+  |> List.map (fun tag -> (tag_id_map |> StringMap.find tag, tag))
+
+let dump_account_tag_records (model : Model.t) account_id_map tag_id_map =
+  (* (account_id, tag_id) list *)
+  model.accounts
+  |> List.map (fun (a : Model.open_account) ->
+         let a_id = account_id_map |> StringMap.find (list_last a.account) in
+         a.tags
+         |> List.map (fun tag ->
+                let tag_id = tag_id_map |> StringMap.find tag in
+                (a_id, tag_id)))
+  |> List.flatten |> List.sort_uniq compare
+
+let dump_transaction_records_and_posting_records (model : Model.t)
+    account_id_map =
+  (* (id, date, narration) list *)
+  (* (id, account_id, transaction_id, amount, narration) list *)
+  model.transactions
+  |> List.fold_left
+       (fun (tx_recs, ps_recs) (tx : Model.transaction) ->
+         let tx_id = List.length tx_recs in
+         let tx_recs' =
+           (tx_id, Store.string_of_date tx.date, tx.narration) :: tx_recs
+         in
+         let ps_recs' =
+           (tx.postings
+           |> List.mapi (fun i (p : Model.posting) ->
+                  let ps_id = List.length ps_recs + i in
+                  let acc_id =
+                    account_id_map |> StringMap.find (list_last p.account)
+                  in
+                  (ps_id, acc_id, tx_id, Option.get p.amount, p.narration)))
+           @ ps_recs
+         in
+         (tx_recs', ps_recs'))
+       ([], [])
+
+let dump_transaction_tag_records (model : Model.t) tag_id_map =
+  (* (transaction_id, tag_id) list *)
+  model.transactions
+  |> List.mapi (fun i (t : Model.transaction) ->
+         t.tags |> List.map (fun tag -> (i, tag_id_map |> StringMap.find tag)))
+  |> List.flatten |> List.sort_uniq compare
+
+let dump_csv (model : Model.t) =
+  let account_id_map = dump_account_id_map model in
+  let tag_id_map = dump_tag_id_map model in
+  let account_records = dump_account_records model account_id_map in
+  let tag_records = dump_tag_records model tag_id_map in
+  let account_tag_records =
+    dump_account_tag_records model account_id_map tag_id_map
+  in
+  let transaction_records, posting_records =
+    dump_transaction_records_and_posting_records model account_id_map
+  in
+  let transaction_tag_records = dump_transaction_tag_records model tag_id_map in
+
+  let oc = open_out "accounts.csv" in
+  account_records
+  |> List.iter (fun (id, name, currency, parent_id, kind) ->
+         Printf.fprintf oc "%d,%s,%s,%s,%d\n" id name currency
+           (parent_id
+           |> Option.fold ~none:"NULL" ~some:(fun id -> string_of_int id))
+           (Model.int_of_account_kind kind));
+  close_out oc;
+
+  let oc = open_out "tags.csv" in
+  tag_records
+  |> List.iter (fun (id, name) -> Printf.fprintf oc "%d,%s\n" id name);
+  close_out oc;
+
+  let oc = open_out "account_tags.csv" in
+  account_tag_records
+  |> List.iter (fun (account_id, tag_id) ->
+         Printf.fprintf oc "%d,%d\n" account_id tag_id);
+  close_out oc;
+
+  let oc = open_out "transactions.csv" in
+  transaction_records
+  |> List.iter (fun (id, date, narration) ->
+         Printf.fprintf oc "%d,%s,%s\n" id date narration);
+  close_out oc;
+
+  let oc = open_out "postings.csv" in
+  posting_records
+  |> List.iter (fun (id, account_id, transaction_id, amount, narration) ->
+         Printf.fprintf oc "%d,%d,%d,%d,%s\n" id account_id transaction_id
+           amount narration);
+  close_out oc;
+
+  let oc = open_out "transaction_tags.csv" in
+  transaction_tag_records
+  |> List.iter (fun (transaction_id, tag_id) ->
+         Printf.fprintf oc "%d,%d\n" transaction_id tag_id);
+  close_out oc;
+
+  ()
+
 let dump uri (model : Model.t) =
   let%lwt con = Caqti_lwt.connect (Uri.of_string uri) >>= Caqti_lwt.or_fail in
   Store.create_accounts con;%lwt
@@ -250,40 +411,128 @@ let dump uri (model : Model.t) =
   Store.create_transaction_tags con;%lwt
   Store.create_account_tags con;%lwt
 
-  (model.accounts
-  |> Lwt_list.iter_s @@ fun (acc : Model.open_account) ->
-     let%lwt acc_id =
-       Store.insert_account con ~account:acc.account ~currency:acc.currency
-         ~kind:(Model.int_of_account_kind acc.kind)
-     in
+  let account_id_map = dump_account_id_map model in
+  let tag_id_map = dump_tag_id_map model in
+  let account_records = dump_account_records model account_id_map in
+  let tag_records = dump_tag_records model tag_id_map in
+  let account_tag_records =
+    dump_account_tag_records model account_id_map tag_id_map
+  in
+  let transaction_records, posting_records =
+    dump_transaction_records_and_posting_records model account_id_map
+  in
+  let transaction_tag_records = dump_transaction_tag_records model tag_id_map in
 
-     acc.tags
-     |> Lwt_list.map_s (fun name ->
-            try%lwt Store.insert_tag con ~name
-            with _ -> Store.select_tag con ~name)
-     >>= Lwt_list.iter_s (fun tag_id ->
-             Store.insert_account_tag con ~account_id:acc_id ~tag_id));%lwt
+  (* Insert accounts *)
+  let%lwt _ =
+    let module C = (val con : Caqti_lwt.CONNECTION) in
+    let open Caqti_request.Infix in
+    let open Caqti_type.Std in
+    C.exec
+      ((unit ->. unit)
+      @@ Printf.sprintf
+           {|INSERT INTO accounts (id, name, currency, parent_id, kind) VALUES %s|}
+           (account_records
+           |> List.map (fun (id, name, currency, parent_id, kind) ->
+                  Printf.sprintf "(%d, '%s', '%s', %s, %d)" id
+                    (escape_single_quote name)
+                    (escape_single_quote currency)
+                    (parent_id
+                    |> Option.fold ~none:"NULL" ~some:(fun i -> string_of_int i)
+                    )
+                    (Model.int_of_account_kind kind))
+           |> String.concat ", "))
+      ()
+    |> Store.raise_if_error
+  in
+
+  (* Insert tags *)
+  let%lwt _ =
+    let module C = (val con : Caqti_lwt.CONNECTION) in
+    let open Caqti_request.Infix in
+    let open Caqti_type.Std in
+    C.exec
+      ((unit ->. unit)
+      @@ Printf.sprintf {|INSERT INTO tags (id, name) VALUES %s|}
+           (tag_records
+           |> List.map (fun (id, name) -> Printf.sprintf "(%d, '%s')" id name)
+           |> String.concat ", "))
+      ()
+    |> Store.raise_if_error
+  in
+
+  (* Insert account_tags *)
+  let%lwt _ =
+    let module C = (val con : Caqti_lwt.CONNECTION) in
+    let open Caqti_request.Infix in
+    let open Caqti_type.Std in
+    C.exec
+      ((unit ->. unit)
+      @@ Printf.sprintf
+           {|INSERT INTO account_tags (account_id, tag_id) VALUES %s|}
+           (account_tag_records
+           |> List.map (fun (account_id, tag_id) ->
+                  Printf.sprintf "(%d, %d)" account_id tag_id)
+           |> String.concat ", "))
+      ()
+    |> Store.raise_if_error
+  in
+
+  (* Insert transactions *)
+  let%lwt _ =
+    let module C = (val con : Caqti_lwt.CONNECTION) in
+    let open Caqti_request.Infix in
+    let open Caqti_type.Std in
+    C.exec
+      ((unit ->. unit)
+      @@ Printf.sprintf
+           {|INSERT INTO transactions (id, created_at, narration) VALUES %s|}
+           (transaction_records
+           |> List.map (fun (id, date, narration) ->
+                  Printf.sprintf "(%d, '%s', '%s')" id date
+                    (escape_single_quote narration))
+           |> String.concat ", "))
+      ()
+    |> Store.raise_if_error
+  in
+
+  (* Insert postings *)
+  let%lwt _ =
+    let module C = (val con : Caqti_lwt.CONNECTION) in
+    let open Caqti_request.Infix in
+    let open Caqti_type.Std in
+    C.exec
+      ((unit ->. unit)
+      @@ Printf.sprintf
+           {|INSERT INTO postings (id, account_id, transaction_id, amount, narration) VALUES %s|}
+           (posting_records
+           |> List.map
+                (fun (id, account_id, transaction_id, amount, narration) ->
+                  Printf.sprintf "(%d, %d, %d, %d, '%s')" id account_id
+                    transaction_id amount narration)
+           |> String.concat ", "))
+      ()
+    |> Store.raise_if_error
+  in
+
+  (* Insert transaction_tags *)
+  let%lwt _ =
+    let module C = (val con : Caqti_lwt.CONNECTION) in
+    let open Caqti_request.Infix in
+    let open Caqti_type.Std in
+    C.exec
+      ((unit ->. unit)
+      @@ Printf.sprintf
+           {|INSERT INTO transaction_tags (transaction_id, tag_id) VALUES %s|}
+           (transaction_tag_records
+           |> List.map (fun (transaction_id, tag_id) ->
+                  Printf.sprintf "(%d, %d)" transaction_id tag_id)
+           |> String.concat ", "))
+      ()
+    |> Store.raise_if_error
+  in
 
   Store.create_full_accounts_view con;%lwt
-
-  (model.transactions
-  |> Lwt_list.iter_s @@ fun (tx : Model.transaction) ->
-     let%lwt tx_id =
-       Store.insert_transaction con ~date:tx.date ~narration:tx.narration
-     in
-
-     tx.tags
-     |> Lwt_list.map_s (fun name ->
-            try%lwt Store.insert_tag con ~name
-            with _ -> Store.select_tag con ~name)
-     >>= Lwt_list.iter_s (fun tag_id ->
-             Store.insert_transaction_tag con ~transaction_id:tx_id ~tag_id);%lwt
-
-     tx.postings
-     |> Lwt_list.iter_s @@ fun (p : Model.posting) ->
-        let%lwt account_id = Store.select_account_by_fullname con p.account in
-        Store.insert_posting con ~account_id ~transaction_id:tx_id
-          ~amount:(Option.get p.amount) ~narration:p.narration);%lwt
 
   (model.accounts
   |> Lwt_list.iter_s @@ fun (acc : Model.open_account) ->
